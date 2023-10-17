8ffdf1051ba55921b78644ede0d62e0f0f81cff2
Esmail3576 <esmaeel3576@gmail.com>
2023-10-17 13:59:32 -0700

    add _printf.c

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _printf - that produces output according to a format.
+ * @format: is a character string.
+ * @...: arguments
+ * Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	EB a[] = {
+		{"%c", _char},
+		{"%s", _string},
+		{"%%", _percent},
+	};
+
+	va_list list_args;
+	int j, i = 0, num = 0;
+
+	va_start(list_args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);/*operation failed*/
+
+	while (format[i] != '\0')
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (a[j].it[0] == format[i] && a[j].it[1] == format[i + 1])
+			{
+				num += a[j].fun(list_args);
+				i += 2;
+			}
+		}
+		_putchar(format[i]);
+		i++;
+		num++;
+	}
+	va_end(list_args);
+	return (num);
+}
