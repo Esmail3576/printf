12c881282a7b3a465a8649eba9d7b6d7ae1ee1d9
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-17 14:09:12 -0700

    add _percent.c

---
+++ _percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _percent - print '%'
+ * @per: argument
+ * Return: int
+*/
+
+int _percent(__attribute__((unused)) va_list per)
+{
+	/* Ascii code % = 37 */
+	_putchar(37);
+	/* putchar print 1 character */
+	return (1);
+}
